0a4c746316c90d0ce274c0b60ddd3054e4a5f7e5
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-11-02 18:41:11 -0300

    Completa micro desafío 1

---
+++ manipulacion-y-consultas.sql
@@ -0,0 +1,78 @@
+/* Micro desafío - Paso 1:
+Utilizando el Insert , Update , Delete , debemos ejecutar lo siguiente: */
+
+/* 1. Insertar en la tabla genres un nuevo género con los siguientes datos:
+name: Investigación
+ranking: 13
+active: 1 */
+INSERT INTO genres (name, ranking, active)
+VALUES ("Investigación", 13, 1)
+
+/* 2. Actualizar el nuevo registro “name: Investigación” por “Investigación Científica”. */
+
+UPDATE genres
+SET name = "Investigación Científica"
+WHERE name = "Investigación"
+
+/* 3. Eliminar el registro cuyo name es: “Investigación Científica”. Recordemos verificar
+cuál es el id de dicho registro. */
+
+DELETE FROM genres 
+WHERE id = 13
+
+
+/* 4. Mostrar todos los registros de la tabla “movies”. */
+
+SELECT * FROM movies 
+
+/* 5. Mostrar el nombre, apellido y rating de todos los actores. */
+
+SELECT first_name, last_name, rating
+FROM actors
+
+/* 6. Mostrar el título de todas las series. Tomar en cuenta que tanto el nombre de la
+tabla como el campo estén en español. */
+
+
+SELECT title AS Título
+FROM series
+
+
+
+
+
+/* Micro desafío - Paso 2:
+Utilizando el Where y Order by , ejecutemos las siguientes consultas (ten en cuenta el uso
+de los operadores lógicos y relacionales). */
+
+/* 1. Mostrar el nombre y apellido de los actores cuyo rating sea mayor a 7.5. */
+
+/* 2. Mostrar el título de las películas, el rating y los premios de las películas con un rating
+mayor a 7.5 y con más de dos premios. */
+
+/* 3. Mostrar el título de las películas y el rating ordenadas por rating en forma
+ascendente. */
+
+
+
+
+/* Micro desafío - Paso 3: */
+
+/* 1. Mostrar los títulos de las primeras tres películas en la base de datos. */
+
+/* 2. Mostrar el top 5 de las películas con mayor rating. */
+
+/* 3. Mostrar las top 5 a 10 de las películas con mayor rating. */
+
+/* 4. Listar los primeros 10 actores (sería la página 1).
+a. Luego, usar offset para traer la página 3. */
+
+
+
+/* Micro desafío - Paso 4: */
+
+/* 1. Mostrar el título y rating de todas las películas cuyo título sea Harry Potter. */
+
+/* 2. Mostrar a todos los actores cuyos nombres empiecen con Sam. */
+
+/* 3. Mostrar el título de las películas que salieron entre el 2004 y 2008. */
