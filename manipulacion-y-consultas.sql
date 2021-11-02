7a476d3f9c9baf02d1d03d5d1a33074bf2f2a042
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-11-02 18:47:04 -0300

    Completa micro desafío 2

--- manipulacion-y-consultas.sql
+++ manipulacion-y-consultas.sql
@@ -47,14 +47,20 @@ de los operadores lógicos y relacionales). */
 
 /* 1. Mostrar el nombre y apellido de los actores cuyo rating sea mayor a 7.5. */
 
+SELECT first_name, last_name FROM actors
+WHERE rating > 7.5
+
 /* 2. Mostrar el título de las películas, el rating y los premios de las películas con un rating
 mayor a 7.5 y con más de dos premios. */
+
+SELECT title, rating, awards FROM movies
+WHERE rating > 7.5 AND awards > 2
 
 /* 3. Mostrar el título de las películas y el rating ordenadas por rating en forma
 ascendente. */
 
-
-
+SELECT title, rating FROM movies
+ORDER BY rating
 
 /* Micro desafío - Paso 3: */
 
